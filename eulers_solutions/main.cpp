94274952b1d1378d9adf2b4f444c2f971eabf677
Rohan <rohan_poundarik@trimble.com>
2016-08-02 11:37:18 -0700

    solved problem 6, sum square difference

--- eulers_solutions/main.cpp
+++ eulers_solutions/main.cpp
@@ -190,6 +190,35 @@ What is the smallest positive number that is evenly divisible by all of the numb
    }
 
 
+
+
+/** \brief
+ *The sum of the squares of the first ten natural numbers is,
+ 1^2 + 2^2 + ...+ 10^2 = 385
+ The square of the sum of the first ten natural numbers is,
+ (1+2+...+10)^2 = 55^2 =3025
+  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 -385 = 2640
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * \param: none
+ * \return: the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+
+int sumOfFirstHundNN()
+{
+    int sum1=0,sum2 =0;
+
+    for (int i=1 ;i <=100;i++)
+    {
+        sum1 = sum1 + i*i;
+        sum2 = sum2 + i;
+
+    }
+    sum2 = sum2 * sum2;
+
+    return sum2 - sum1;
+}
+
+
 int main()
 {
     cout<<"---------------------------------------------------------------------"<<endl;
@@ -217,10 +246,14 @@ int main()
     cout<< "Problem 4: Largest palindrome made from the product of two 3-digit numbers is: "<<endl;
     cout<< largestPalindromeProduct() <<endl;
     cout<<"---------------------------------------------------------------------"<<endl;
-    */
+
 
     cout<< "Problem 5: The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is: "<<endl;
     cout<< sNoDByOneToTwenty() <<endl;
+    cout<<"---------------------------------------------------------------------"<<endl;
+*/
+    cout<< "Problem 6: The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is: "<<endl;
+    cout<< sumOfFirstHundNN() <<endl;
     cout<<"---------------------------------------------------------------------"<<endl;
 
 
